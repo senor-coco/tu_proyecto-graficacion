d6d2aa25369bceb70b7ac365b8658cbdf940fa3d
coco <130810567+senor-coco@users.noreply.github.com>
2025-02-19 15:49:12 -0600

    Ya Xb y pusimos un tabla de almacen

--- app.py
+++ app.py
@@ -3,48 +3,58 @@ import plotly.graph_objects as go
 
 app = Flask(__name__)
 
-# Lista global para almacenar los puntos y líneas
-data = {"points": [], "lines": []}
+# Lista global para almacenar las líneas generadas
+data = {"lines": []}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/add_point', methods=['POST'])
-def add_point():
-    global data
-    point = request.json  # Recibe JSON con x, y, z
-    data["points"].append(point)
-    return jsonify(data)
-
 @app.route('/add_line', methods=['POST'])
 def add_line():
     global data
-    line = request.json  # Recibe JSON con dos puntos (inicio y fin)
-    data["lines"].append(line)
+    params = request.json  
+    xa, ya, xb, yb = params["xa"], params["ya"], params["xb"], params["yb"]
+
+    # Algoritmo DDA
+    dx = xb - xa
+    dy = yb - ya
+    steps = max(abs(dx), abs(dy))
+    x_inc = dx / steps
+    y_inc = dy / steps
+
+    x, y = xa, ya
+    x_values, y_values = [], []
+
+    for _ in range(int(steps) + 1):
+        x_values.append(round(x))
+        y_values.append(round(y))
+        x += x_inc
+        y += y_inc
+
+    data["lines"].append({
+        "xa": xa, "ya": ya, "xb": xb, "yb": yb, 
+        "x_values": x_values, "y_values": y_values
+    })
+    
     return jsonify(data)
 
 @app.route('/get_data', methods=['GET'])
 def get_data():
     return jsonify(data)
 
+@app.route('/delete_line/<int:index>', methods=['POST'])
+def delete_line(index):
+    global data
+    if 0 <= index < len(data["lines"]):
+        del data["lines"][index]
+    return jsonify(data)
+
 @app.route('/clear_data', methods=['POST'])
 def clear_data():
     global data
-    data = {"points": [], "lines": []}  # Reiniciar datos
-    return jsonify({"message": "Datos limpiados"})
-
-def create_plot():
-    fig = go.Figure()
-    for point in data["points"]:
-        fig.add_trace(go.Scatter3d(x=[point["x"]], y=[point["y"]], z=[point["z"]],
-                                   mode='markers', marker=dict(size=5, color='red')))
-    for line in data["lines"]:
-        fig.add_trace(go.Scatter3d(x=[line["start"]["x"], line["end"]["x"]],
-                                   y=[line["start"]["y"], line["end"]["y"]],
-                                   z=[line["start"]["z"], line["end"]["z"]],
-                                   mode='lines', line=dict(color='blue', width=2)))
-    return fig.to_html(full_html=False)
+    data = {"lines": []}
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
